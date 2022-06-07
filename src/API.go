53132c80676883abe3589d2676c34b825158271f
Zach Hildreth <41015215+zachhildreth@users.noreply.github.com>
2022-06-07 14:35:01 -0700

    Update API.go

--- src/API.go
+++ src/API.go
@@ -1,95 +1,16 @@
 package main
  
 import (
-"fmt"
-"strconv"
-"bytes"
-"io/ioutil"
-"net/http"
-"time"
 "github.com/gofiber/fiber/v2"
 )
- 
-// global structures 
 
-type ErrorResults struct {
-    Error string `json:"Error"`
-}
- 
-// global constants
-const URL = "http://162.55.235.87/?id="
-const TX_HASH_LENGTH = 64
-const PUBLIC_ADDRESS_LENGTH = 98
- 
-// Functions
-func send_http_data(url string,data string) string {
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(data)))
-        if err != nil {
-	  return "error1"
-	}
-  
-	req.Header.Set("Content-Type", "application/json")
- 
-	client := &http.Client{}
-	client.Timeout = time.Second * 2
-	resp, err := client.Do(req)
-        if err != nil {
-	  return "error1"
-	}
-	defer resp.Body.Close()
-        body, _ := ioutil.ReadAll(resp.Body)
-fmt.Printf("for %s sending %s received %s\n", url,data,body)
+/*
+global structures in src/structures.go
+global constants in src/constants.go
+network functions in src/network_functions.go
+blockchain API in src/API_blockchain.go
+*/
 
-        return string(body)
-}
- 
-
-func get_http_data(url string) string {
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer([]byte("")))
-        if err != nil {
-	  return "error1"
-	}
-  
-req.Header.Set("Content-Type", "application/json")
- 
-	client := &http.Client{}
-	client.Timeout = time.Second * 2
-	resp, err := client.Do(req)
-        if err != nil {
-	  return "error1"
-	}
-	defer resp.Body.Close()
-        body, _ := ioutil.ReadAll(resp.Body)
-        return string(body)
-}
-
-
-
-func helloWorld(c *fiber.Ctx) error {
-	 // Variables
-    var id string
-    tx_hash := c.Query("tx_hash")
-    amount := c.Query("amount")
-
-    // error check
-    if (len(tx_hash) != TX_HASH_LENGTH) {
-      error := ErrorResults{"error"}
-      return c.JSON(error)
-    }
-
-    if _, err := strconv.Atoi(amount); err != nil {
-      error := ErrorResults{"error"}
-      return c.JSON(error)
-    }
-
-    
-fmt.Printf("str1: %s\n", "data")
-
- 
-    // return the id
-    return c.SendString(URL + id + "}")
-}
- 
 func main() {
   
 // setup fiber
@@ -98,11 +19,13 @@ Prefork: true,
 DisableStartupMessage: true,
 })
 
-// setup routes
+// setup blockchain routes
 app.Post("/test/",helloWorld)
-app.Static("/", "/var/www/html/") 
+
+
+// setup global routes
 app.Get("/*", func(c *fiber.Ctx) error {
-  return c.SendString("Invalid URL")
+  return c.SendString("Invalid API Request")
 })
  
   app.Listen(":9000")
