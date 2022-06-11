a3964245a90468f53b3022395ef31755df7d6d49
Zach Hildreth <41015215+zachhildreth@users.noreply.github.com>
2022-06-10 18:13:38 -0700

    Update API.go

--- src/API.go
+++ src/API.go
@@ -57,11 +57,11 @@ func main() {
   app.Get("/v1/xcash/dpops/unauthorized/delegates/online/",v1_xcash_dpops_unauthorized_delegates_online)
   app.Get("/v1/xcash/dpops/unauthorized/delegates/active/",v1_xcash_dpops_unauthorized_delegates_active)
   app.Get("/v1/xcash/dpops/unauthorized/delegates/:delegateName/",v1_xcash_dpops_unauthorized_delegates)
-  //app.Get("/v1/xcash/dpops/unauthorized/delegates/rounds/:delegateName/:start/:limit",v1_xcash_dpops_unauthorized_delegates_rounds)
-  //app.Get("/v1/xcash/dpops/unauthorized/delegates/votes/:delegateName/:start/:limit",v1_xcash_dpops_unauthorized_delegates_votes)
-  //app.Get("/v1/xcash/dpops/unauthorized/votes/:address",v1_xcash_dpops_unauthorized_votes)
-  //app.Get("/v1/xcash/dpops/unauthorized/rounds/:blockHeight",v1_xcash_dpops_unauthorized_rounds)
-  //app.Get("/v1/xcash/dpops/unauthorized/lastBlockProducer",v1_xcash_dpops_unauthorized_last_block_producer)
+  app.Get("/v1/xcash/dpops/unauthorized/delegates/rounds/:delegateName/:start/:limit",v1_xcash_dpops_unauthorized_delegates_rounds)
+  app.Get("/v1/xcash/dpops/unauthorized/delegates/votes/:delegateName/:start/:limit",v1_xcash_dpops_unauthorized_delegates_votes)
+  app.Get("/v1/xcash/dpops/unauthorized/votes/:address",v1_xcash_dpops_unauthorized_votes)
+  app.Get("/v1/xcash/dpops/unauthorized/rounds/:blockHeight",v1_xcash_dpops_unauthorized_rounds)
+  app.Get("/v1/xcash/dpops/unauthorized/lastBlockProducer",v1_xcash_dpops_unauthorized_last_block_producer)
 
 
   // setup global routes
