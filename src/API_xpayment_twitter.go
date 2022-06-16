140cc6027d8ea0d07fcfd0759ee8c42f80224421
Zach Hildreth <41015215+zachhildreth@users.noreply.github.com>
2022-06-15 22:39:25 -0700

    Update API_xpayment_twitter.go

--- src/API_xpayment_twitter.go
+++ src/API_xpayment_twitter.go
@@ -2,11 +2,13 @@ package main
 
 import (
 "strconv"
+"sort"
 "context"
 "time"
 "github.com/gofiber/fiber/v2"
 "go.mongodb.org/mongo-driver/mongo"
 "go.mongodb.org/mongo-driver/bson"
+"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func v1_xpayment_twitter_unauthorized_stats(c *fiber.Ctx) error {
@@ -270,6 +272,113 @@ func v1_xpayment_twitter_unauthorized_statsperday(c *fiber.Ctx) error {
       start = len(output)
     }
     output = output[start:limit]
+
+  return c.JSON(output)
+}
+
+func v1_xpayment_twitter_unauthorized_topstats(c *fiber.Ctx) error {
+
+  // Variables
+  var output v1XpaymentTwitterUnauthorizedTopstats
+  var mongo_sort *mongo.Cursor
+  var mongo_results []bson.M
+  var error error
+  
+  var amount int
+  var total_amount_public int = 0
+  var total_amount_private int = 0
+  var total_volume_public int = 0
+  var total_volume_private int = 0
+  
+
+  // setup database
+  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+  defer cancel()
+  
+   // get the resource
+  if amount,_ = strconv.Atoi(c.Params("amount")); c.Params("amount") == "" || amount < 0 {
+    error := ErrorResults{"Could not get xpayment twitter top stats"}
+    return c.JSON(error)
+  }
+  
+  
+  // get the user details
+  mongo_sort, error = mongoClient.Database(XPAYMENT_TWITTER_DATABASE).Collection("userWallets").Find(ctx, bson.D{{}})
+  if error != nil {
+    error := ErrorResults{"Could not get the xpayment twitter statistics"}
+    return c.JSON(error)
+  }
+
+  if error = mongo_sort.All(ctx, &mongo_results); error != nil {
+    error := ErrorResults{"Could not get the xcash dpops statistics"}
+    return c.JSON(error)
+  }
+  
+  for _, item := range mongo_results {
+	
+	// get the tips
+    tamount_public1,err1 := item["stats"].(primitive.M)["tTipsSent"].(int32)
+    if err1 {
+        total_amount_public = int(tamount_public1)
+    }
+    tamount_public2,err2 := item["stats"].(primitive.M)["tTipsSent"].(int64)
+    if err2 {
+        total_amount_public = int(tamount_public2)
+    }
+    
+    tamount_private1,err3 := item["stats"].(primitive.M)["tTipsSentP"].(int32)
+    if err3 {
+        total_amount_private = int(tamount_private1)
+    }
+    tamount_private2,err4 := item["stats"].(primitive.M)["tTipsSentP"].(int64)
+    if err4 {
+        total_amount_private = int(tamount_private2)
+    }
+    
+	data := TopTips{item["userName"].(string),total_amount_private + total_amount_public}
+    output.TopTips = append(output.TopTips,data)
+    
+    // get the total volume
+    tvolume_public1,err5 := item["stats"].(primitive.M)["tVolSent"].(int32)
+    if err5 {
+        total_volume_public = int(tvolume_public1)
+    }
+    tvolume_public2,err6 := item["stats"].(primitive.M)["tVolSent"].(int64)
+    if err6 {
+        total_volume_public = int(tvolume_public2)
+    }
+    
+    tvolume_private1,err7 := item["stats"].(primitive.M)["tVolSentP"].(int32)
+    if err7 {
+        total_volume_private = int(tvolume_private1)
+    }
+    tvolume_private2,err8 := item["stats"].(primitive.M)["tVolSentP"].(int64)
+    if err8 {
+        total_volume_private = int(tvolume_private2)
+    }
+    
+	data1 := TopVolumes{item["userName"].(string),total_volume_public + total_volume_private}
+    output.TopVolumes = append(output.TopVolumes,data1)
+  }
+  
+  // sort the array
+  sort.Slice(output.TopTips[:], func(i, j int) bool {
+        return output.TopTips[i].Tips > output.TopTips[j].Tips
+    })
+    sort.Slice(output.TopVolumes[:], func(i, j int) bool {
+        return output.TopVolumes[i].Volume > output.TopVolumes[j].Volume
+    })
+	
+	// only return the amount
+    if amount > len(output.TopTips) {
+      amount = len(output.TopTips)
+    }
+    output.TopTips = output.TopTips[0:amount]
+    
+    if amount > len(output.TopVolumes) {
+      amount = len(output.TopVolumes)
+    }
+    output.TopVolumes = output.TopVolumes[0:amount]
 
   return c.JSON(output)
 }
